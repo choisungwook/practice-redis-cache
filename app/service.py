30889ca7b828cf53a6eb8fba86d6b8f8c7af475c
choisungwook <kgg1959@naver.com>
2022-08-24 00:12:49 +0900

    redis에 값 쓰는 로직 추가

--- app/service.py
+++ app/service.py
@@ -1,6 +1,18 @@
 from redis_connection import get_redis_connection
 import requests
 from fastapi import HTTPException
+from pydantic import BaseSettings
+from typing import Optional
+
+
+class CountryInfo(BaseSettings):
+    domains: str
+    alpha_two_code: str
+    country: str
+    web_pages: str
+    name: str
+    state_province: Optional[str] = ''
+
 
 class CountryService:
     def __init__(self):
@@ -19,6 +31,7 @@ class CountryService:
 
     def __get_university_from_redis(self, country_name: str):
         """redis에 university가 있는지 확인"""
+        # scan 0 MATCH Turkey* count 300
         return None
 
     def __get_university_from_restapi(self, country_name: str) -> list:
@@ -36,4 +49,17 @@ class CountryService:
 
     def __add_university_to_redis(self, countries: list):
         """redis에 university 추가"""
-        pass
+        for country in countries:
+            country_info = CountryInfo(
+                domains="@".join(country["domains"]),
+                alpha_two_code=country["alpha_two_code"],
+                country=country["country"],
+                web_pages="@".join(country["web_pages"]),
+                name=country["name"],
+                state_province=self.__is_value_none_return_emptystring(country["state-province"])
+            )
+            key = f"{country_info.country}:{country_info.name.replace(' ', '_')}"
+            self.redis_conn.hmset(key, country_info.dict())
+
+    def __is_value_none_return_emptystring(self, value):
+        return value or ""
